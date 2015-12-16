45719c04b849bfe132a1f7d646ef15b009d53053
Shinya Sakemoto <sakebook@gmail.com>
2015-12-16 20:30:37 +0900

    Modify dimen

--- app/src/main/java/com/sakebook/android/sample/parallaxsample/MainActivity.java
+++ app/src/main/java/com/sakebook/android/sample/parallaxsample/MainActivity.java
@@ -2,9 +2,11 @@ package com.sakebook.android.sample.parallaxsample;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.SparseArray;
 import android.view.View;
 
@@ -32,7 +34,7 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
         FragmentManager fragmentManager = getSupportFragmentManager();
         adapter = new ParallaxPagerAdapter(fragmentManager, this, makeFragments());
         viewPager.setAdapter(adapter);
-        viewPager.setPageMargin(16);
+        viewPager.setPageMargin(getResources().getDimensionPixelSize(R.dimen.small_margin));
 //        viewPager.setPageMarginDrawable(R.mipmap.ic_launcher);
 //        viewPager.setOffscreenPageLimit(4);
         viewPager.addOnPageChangeListener(this);
