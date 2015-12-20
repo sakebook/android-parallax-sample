ba6c0c60742c88ec5099539e3200b7beb36ddfe2
Shinya Sakemoto <sakebook@gmail.com>
2015-12-20 18:14:34 +0900

    Add controll text

--- app/src/main/java/com/sakebook/android/sample/parallaxsample/acitvities/IndicatorActivity.java
+++ app/src/main/java/com/sakebook/android/sample/parallaxsample/acitvities/IndicatorActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.SparseArray;
 import android.view.View;
+import android.widget.TextView;
 
 import com.sakebook.android.sample.parallaxsample.views.adapters.ParallaxPagerAdapter;
 import com.sakebook.android.sample.parallaxsample.views.ParallaxTransformer;
@@ -17,21 +18,22 @@ import com.sakebook.android.sample.parallaxsample.fragments.FourthFragment;
 import com.sakebook.android.sample.parallaxsample.fragments.SecondFragment;
 import com.sakebook.android.sample.parallaxsample.fragments.ThirdFragment;
 
-public class IndicatorActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener{
+public class IndicatorActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener, View.OnClickListener {
 
     private ViewPager viewPager;
     private ParallaxPagerAdapter adapter;
     private SparseArray<Fragment> viewSparseArray = new SparseArray<>();
     private View indicatorFloatView;
     private float indicatorSpace;
+    private TextView back;
+    private TextView next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_indicator);
         initViewPager();
-        setupIndicatorSpace();
-        indicatorFloatView = findViewById(R.id.view_float_indicator);
+        setupIndicator();
     }
 
     private void initViewPager() {
@@ -53,9 +55,15 @@ public class IndicatorActivity extends AppCompatActivity implements ViewPager.On
         return viewSparseArray;
     }
 
-    private void setupIndicatorSpace() {
+    private void setupIndicator() {
         float scale = getResources().getDisplayMetrics().density;
         indicatorSpace = getResources().getDimensionPixelSize(R.dimen.middle_margin);// * scale;
+        indicatorFloatView = findViewById(R.id.view_float_indicator);
+        back = (TextView) findViewById(R.id.text_indicator_back);
+        next = (TextView) findViewById(R.id.text_indicator_next);
+        back.setOnClickListener(this);
+        next.setOnClickListener(this);
+
     }
 
     @Override
@@ -65,11 +73,44 @@ public class IndicatorActivity extends AppCompatActivity implements ViewPager.On
 
     @Override
     public void onPageSelected(int position) {
+        if (position == 0) {
+            back.setText("skip");
+        } else {
+            back.setText("back");
+        }
 
+        if (viewSparseArray.size() == (position + 1)) {
+            next.setText("Go!");
+        } else {
+            next.setText("next");
+        }
     }
 
     @Override
     public void onPageScrollStateChanged(int state) {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.text_indicator_next) {
+            next();
+        } else if (v.getId() == R.id.text_indicator_back) {
+            back();
+        }
+    }
+
+    private void next() {
+        if (viewPager.getCurrentItem() != viewSparseArray.size()) {
+            viewPager.setCurrentItem(viewPager.getCurrentItem() + 1, true);
+            return;
+        }
+    }
+
+    private void back() {
+        if (viewPager.getCurrentItem() == 0) {
+            return;
+        }
+        viewPager.setCurrentItem(viewPager.getCurrentItem() - 1, true);
     }
 }
