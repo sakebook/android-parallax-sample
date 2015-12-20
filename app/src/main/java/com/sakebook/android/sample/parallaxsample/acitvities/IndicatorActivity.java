1b211e71669336eb830137c9957e8622869921e5
Shinya Sakemoto <sakebook@gmail.com>
2015-12-20 17:25:11 +0900

    Add indicator sample

--- app/src/main/java/com/sakebook/android/sample/parallaxsample/acitvities/IndicatorActivity.java
+++ app/src/main/java/com/sakebook/android/sample/parallaxsample/acitvities/IndicatorActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.SparseArray;
+import android.view.View;
 
 import com.sakebook.android.sample.parallaxsample.views.adapters.ParallaxPagerAdapter;
 import com.sakebook.android.sample.parallaxsample.views.ParallaxTransformer;
@@ -21,12 +22,16 @@ public class IndicatorActivity extends AppCompatActivity implements ViewPager.On
     private ViewPager viewPager;
     private ParallaxPagerAdapter adapter;
     private SparseArray<Fragment> viewSparseArray = new SparseArray<>();
+    private View indicatorFloatView;
+    private float indicatorSpace;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_indicator);
         initViewPager();
+        setupIndicatorSpace();
+        indicatorFloatView = findViewById(R.id.view_float_indicator);
     }
 
     private void initViewPager() {
@@ -46,13 +51,16 @@ public class IndicatorActivity extends AppCompatActivity implements ViewPager.On
         viewSparseArray.put(3, new FourthFragment());
         viewSparseArray.put(4, new FifthFragment());
         return viewSparseArray;
-
     }
 
+    private void setupIndicatorSpace() {
+        float scale = getResources().getDisplayMetrics().density;
+        indicatorSpace = getResources().getDimensionPixelSize(R.dimen.middle_margin);// * scale;
+    }
 
     @Override
     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
+        indicatorFloatView.setTranslationX((position * indicatorSpace) + (positionOffset * indicatorSpace));
     }
 
     @Override
